76fa9b9e77c6e8e3cee0bdad81a0732617f6f8d8
Relativelie <sopio1992@gmail.com>
2022-05-09 22:19:44 +0300

    done

--- tests/__tests__/components/search/Search.test.tsx
+++ tests/__tests__/components/search/Search.test.tsx
@@ -1,10 +1,19 @@
-import { Search } from '../../../../src/components/search/Search';
+import { fireEvent, screen } from '@testing-library/react';
 import { render } from '../../../testsSetup/test-utils';
+import { Search } from '../../../../src/components/search/Search';
 import { data } from '../../../testValues/data';
 
 describe('Search component', () => {
     test('snapshot - Search component', () => {
         const tree = render(<Search data={data} />);
         expect(tree).toMatchSnapshot();
+    });
+
+    test('type some value', () => {
+        render(<Search data={data} />);
+        const inputField = screen.getByPlaceholderText(/поиск/i);
+        fireEvent.change(inputField, { target: { value: 'Harribo' } });
+        fireEvent.focusOut(inputField);
+        expect(inputField).toHaveValue('Harribo');
     });
 });
